06e98e28f7316f57da6bec1ec90eaf17f7ada323
Yuriy Korman <yuriy.korman@sigmaukraine.com>
2016-03-29 03:50:24 +0300

    change js

--- jjpanda.js
+++ jjpanda.js
@@ -1,9 +1,11 @@
 (function($){
 	$('document').ready(function(){
+		var baseUrl = 'https://beta.jjpanda.com';
+
 		$('.button.send').removeAttr('onclick');
 		$('#statistic').click(function(e){
 			var email 	= $('.username').text();
-			window.location = 'https://beta.jjpanda.com/web-mail/statistic/emails?email=' + email;
+			window.location = baseUrl + '/web-mail/statistic/emails?email=' + email;
 					
 			e.preventDefault();
 		});
@@ -12,29 +14,46 @@
 			console.log('test');
 			console.log(this);
 			console.log($(this));
-			
-			var toList = $('#_to').val();
-			
+
+			var self 		= this; 			
+			var fromEmail 	= $('.username').text();
+			var toStr   	= $('#_to').val();
+			var toList  	= toStr.split(',');
+			var toEmail 	= toList[0];
+
+			$.ajax({
+				url: baseUrl + '/web-mail/track/sent',
+				dataType: 'jsonp',
+				method: 'post',
+				data: {
+					to_email: to_email,
+					from_email: from_email	
+				},
+				success: function(data){
+					var id = data.statisticId;
+					var content = '';
+
+					if (tinyMCE.activeEditor) {
+						content = tinyMCE.activeEditor.getContent();
+						tinyMCE.activeEditor.setContent(content + '<img src="' + baseUrl + '/web-mail/track/open?to_email='+toList[i]+'&statistic_id=' + id + '" />');
+					} else {
+						content = $('#composebody').val();
+						$('#composebody').val(content + '<img src="' + baseUrl + '/web-mail/track/open?to_email='+toList[i]+'&statistic_id=' + id + '"/>');
+					}
+					rcmail.command('send','', this, e.originalEvent);
+				},
+				error: function(error){
+					console.log(error);		
+				}
+			});
+
 			for (var i = 0; i < toList.length; i++) {
 				
-				if (tinyMCE.activeEditor) {
-					var content = tinyMCE.activeEditor.getContent();
-					tinyMCE.activeEditor.setContent(content + '<img src="http://dev.panda/web-mail/track/open?to_email='+toList[i]+'&statistic_id=3" />');
-				} else {
-					var text = $('#composebody').val();
-					$('#composebody').val(text + '<img src="http://dev.panda/web-mail/track/open?to_email='+toList[i]+'&statistic_id=3"/>');
-				}
+
 			
 			}
 			
-			if (tinyMCE.activeEditor) {
-				var content = tinyMCE.activeEditor.getContent();
-				tinyMCE.activeEditor.setContent(content + '<img src="http://dev.panda/web-mail/track/open?to_email=korman_@mail.ru&statistic_id=3" />');
-			} else {
-				var text = $('#composebody').val();
-				$('#composebody').val(text + '<img src="http://dev.panda/web-mail/track/open?to_email=korman_@mail.ru&statistic_id=3"/>');
-			}
-			return rcmail.command('send','', this, e.originalEvent);
+			e.preventDefault();
 		});
 	});
 })(jQuery);
